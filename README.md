06a79a64283d6c7b1c38ac72452e6b278a421b2e
Bored <treeclan12@gmail.com>
2021-03-18 19:42:52 -0400

    Version 1

---
+++ README.md
@@ -0,0 +1,15 @@
+# nhentai_dl_script
+
+A really, really, lazy way to download and prep images for use with [hent-AI](https://github.com/natethegreate/hent-AI) and [DeepCreamPy](https://github.com/deeppomf/DeepCreamPy).
+
+## What does it do?
+
+Uses the NHentai API to scrape all the actual images of a given doujin ID. Can also convert those images from jpg (nHentai default) to png for you to use with...certain AI scripts...
+
+## Usage
+
+1. ```pip3 install -r requirements.txt```
+
+2. ```python3 nhentai_dl.py```
+
+3. It'll walk you through the rest!
---
+++ nhentai_dl.py
@@ -0,0 +1,118 @@
+import os, shutil
+import wget
+from NHentai import NHentai
+from PIL import Image
+
+# Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0
+# https://i.nhentai.net/galleries/732189/1.jpg
+
+def __get_sacred_number():
+    sacred_num = input("Enter the number: ")
+    print("--------------------------------------------------------")
+    confirm = input("You entered: " + sacred_num + ". Is this correct? (Y/N): ")
+    print("--------------------------------------------------------")
+    if(confirm == "Y" or confirm == "y" or confirm == "yes" or confirm == "Yes"):
+        return sacred_num
+    else:
+       sacred_num =  __get_sacred_number()
+       return sacred_num
+
+def __get_conversion_answer():
+    convBool = input("Would you like to convert from JPG to PNG for easier use with certain other software? (Y/N): ")
+    print("--------------------------------------------------------")
+    if(convBool == "Y" or convBool == "y" or convBool == "yes" or convBool == "Yes"):
+        return True
+    elif(convBool == "N" or convBool == "n" or convBool == "no" or convBool == "No"):
+        return False
+    else:
+        print("Unable to understand input. Type (Y/y/yes/Yes or N/n/no/No).")
+        print("--------------------------------------------------------")
+        redo = __get_conversion_answer()
+        return redo
+
+def __get_links(sacred_num):
+    nhentai = NHentai()
+    doujin = nhentai._get_doujin(id=f'{sacred_num}')
+    if(doujin == None):
+        raise Exception("This sacred number doesn't exist! Sorry!")
+        exit()
+    else:
+        try:
+            links = doujin.images
+            return links
+        except AttributeError as e:
+            print("An unknown error occured, check your numbers! Stacktrace: ")
+            print(e)
+            print("The program will now exit.")
+            exit()
+    
+
+def __diag(sacred_num, LINK_BASE, id_link):
+    print("Diag s_num: " + sacred_num)
+    print("Diag l_base: " + LINK_BASE)
+    print("Diag id_link: " + id_link)
+
+def __create_dir(sacred_num):
+    path = os.getcwd()
+    path = (path + "/" + str(sacred_num))
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        shutil.rmtree(path)
+        os.mkdir(path)
+    return path
+
+def __download_hentai(sacred_num):
+    links = __get_links(sacred_num)
+    path = __create_dir(sacred_num)
+    
+    for url in links:
+        wget.download(url, path)
+
+def __convert(sacred_num):
+    nhentai = NHentai()
+    doujin = nhentai._get_doujin(id=f'{sacred_num}')
+    try:
+            length = doujin.total_pages
+    except AttributeError as e:
+            print("An unknown error occured, check your numbers! Stacktrace: ")
+            print(e)
+            print("The program will now exit.")
+            exit()
+    i = 1
+    path = os.getcwd()
+    while(i <= length):
+        path = (path + "/" + str(sacred_num) + f"/{i}")
+        try:
+            img = Image.open((path + ".jpg"))
+            img.save((path + ".png"))
+            os.remove((path + ".jpg"))
+        except e:
+            print("An unknown error occured during image conversion The program will exit: ")
+            print()
+            print(e)
+            exit()
+        path = os.getcwd()
+        i = i + 1
+
+def main():
+    LINK_BASE = "https://i.nhentai.net/galleries/"
+    print("--------------------------------------------------------")
+    print("Hello. Welcome to my shitty nHentai downloader script.")
+    print("--------------------------------------------------------")
+    sacred_num = __get_sacred_number()
+    print("We will now (attempt) to download your hentai!")
+    print("--------------------------------------------------------")
+    links = __get_links(sacred_num)
+    __download_hentai(sacred_num)
+    print()
+    print("--------------------------------------------------------")
+    print("Your hentai downloaded SUCCessfully!")
+    print("--------------------------------------------------------")
+    convBool = __get_conversion_answer()
+    if(convBool):
+        __convert(sacred_num)
+    print("Your hentai converted SUCCessfully!")
+
+if __name__ == '__main__':
+    main()
